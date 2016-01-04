b4da7df4552ec65ee06755b55a6be43fa8a70273
Kendra Keith <kendra.d.keith@gmail.com>
2016-01-04 10:55:44 -0500

    add volume control

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -170,6 +170,18 @@
             }
         };
         
+        /**
+        * @function setVolume
+        * @desc Set volume for songs
+        * @param {Number} volume
+        */
+        SongPlayer.setVolume = function(volume) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setVolume(volume);
+            }
+            SongPlayer.volume = volume;
+        };
+        
         return SongPlayer;
     };
  
